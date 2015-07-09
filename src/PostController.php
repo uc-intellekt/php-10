2f01910074d9333c0167d9d391f037674ac5cd02
Victor <bocharsky.bw@gmail.com>
2015-07-09 19:55:35 +0300

    Register Doctrine provider

--- src/PostController.php
+++ src/PostController.php
@@ -2,13 +2,29 @@
 
 class PostController
 {
-    public function indexAction()
+    public function indexAction(\Silex\Application $app)
     {
+        /** @var \Doctrine\DBAL\Connection $conn */
+        $conn = $app['db'];
+
+        $posts = $conn->fetchAll("SELECT * FROM posts");
+
+        print_r($posts);
+
         return 'My blog';
     }
 
-    public function showAction($id)
+    public function showAction(\Silex\Application $app, $id)
     {
+        /** @var \Doctrine\DBAL\Connection $conn */
+        $conn = $app['db'];
+
+        $post = $conn->fetchAssoc("SELECT * FROM posts WHERE id = :id", array(
+            'id' => (int) $id,
+        ));
+
+        print_r($post);
+
         return 'Post ' . $id;
     }
 }
--- web/index.php
+++ web/index.php
@@ -6,6 +6,17 @@ require_once __DIR__.'/../src/PostController.php';
 
 $app = new Silex\Application();
 
+// Register providers
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_mysql',
+        'dbname'   => 'db10',
+        'user'     => 'root',
+        'password' => 'usbw',
+        'port'     => 3307,
+    ),
+));
+
 $app->get('/', 'AppController::indexAction');
 $app->get('/blog', 'PostController::indexAction');
 $app->get('/blog/{id}', 'PostController::showAction');
